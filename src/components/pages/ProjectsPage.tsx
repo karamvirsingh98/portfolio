4e2b919180ed1ea65457895b13f15af3ecd6fa07
karamvirsingh98 <karamvir.singh98@gmail.com>
2021-08-04 18:01:32 -0700

    fix project image src

--- src/components/pages/ProjectsPage.tsx
+++ src/components/pages/ProjectsPage.tsx
@@ -103,42 +103,42 @@ type image = {
 
 const monitor: image[] = [
   {
-    src: "/images/projects/monitor/1.png",
+    src: "./images/projects/monitor/1.png",
     alt: "monitor",
   },
   {
-    src: "/images/projects/monitor/2.png",
+    src: "./images/projects/monitor/2.png",
     alt: "monitor",
   },
   {
-    src: "/images/projects/monitor/3.png",
+    src: "./images/projects/monitor/3.png",
     alt: "monitor",
   },
 ];
 
 const olympaid: image[] = [
   {
-    src: "/images/projects/olympiad/1.png",
+    src: "./images/projects/olympiad/1.png",
     alt: "ethics olympiad",
   },
   {
-    src: "/images/projects/olympiad/2.png",
+    src: "./images/projects/olympiad/2.png",
     alt: "ethics olympiad",
   },
   {
-    src: "/images/projects/olympiad/3.png",
+    src: "./images/projects/olympiad/3.png",
     alt: "ethics olympiad",
   },
   {
-    src: "/images/projects/olympiad/4.png",
+    src: "./images/projects/olympiad/4.png",
     alt: "ethics olympiad",
   },
   {
-    src: "/images/projects/olympiad/5.png",
+    src: "./images/projects/olympiad/5.png",
     alt: "ethics olympiad",
   },
   {
-    src: "/images/projects/olympiad/6.png",
+    src: "./images/projects/olympiad/6.png",
     alt: "ethics olympiad",
   },
 ];
